574ec37366f9b9318e7ce35f3a9742441c436e4b
Lucas Costa <lnatanfrancacosta@gmail.com>
2022-08-07 15:41:46 -0300

    add java based config

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,50 @@
+package guru.springframework.sfgdi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.I18nSpanishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
+import guru.springframework.sfgdi.services.PropertyGreetingService;
+import guru.springframework.sfgdi.services.SetterGreetingService;
+
+@Configuration
+public class GreetingServiceConfig {
+    
+    @Bean
+    ConstructorGreetingService constructorGreetingService() {
+        return new ConstructorGreetingService();
+    }
+
+    @Bean
+    PropertyGreetingService propertyGreetingService() {
+        return new PropertyGreetingService();
+    }
+
+    @Bean
+    SetterGreetingService setterGreetingService() {
+        return new SetterGreetingService();
+    }
+
+    @Bean
+    @Primary
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
+
+    @Bean
+    @Profile({"EN", "default"})
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    } 
+
+    @Bean("i18nService")
+    @Profile({"ES"})
+    I18nSpanishGreetingService i18nSpanishGreetingService() {
+        return new I18nSpanishGreetingService();
+    }
+}
--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,10 +1,6 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
-@Service("i18nService")
-@Profile({"EN", "default"})
 public class I18nEnglishGreetingService implements GreetingService{
 
     @Override
